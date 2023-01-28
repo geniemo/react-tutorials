ddc85e3719d27c065f319cfa4b77dafbf05af36d
geniemo <kor98won@gmail.com>
2023-01-28 16:18:19 +0900

    배열에서 데이터 제거하기

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -4,21 +4,26 @@ import PhoneInfoList from './components/PhoneInfoList';
 
 function App() {
 
-  let id = 0;
+  const [id, setId] = useState(0);
   const [information, setInformation] = useState([]);
 
   const handleCreate = (data) => {
     setInformation(information.concat(Object.assign(
       {},
       data,
-      { id: id++ }
+      { id: id }
     )));
+    setId(id + 1);
   };
+
+  const handleRemove = (id) => {
+    setInformation(information.filter(info => info.id !== id));
+  }
 
   return (
     <div>
       <PhoneForm onCreate={handleCreate} />
-      <PhoneInfoList data={information} />
+      <PhoneInfoList data={information} onRemove={handleRemove} />
     </div>
   );
 }
